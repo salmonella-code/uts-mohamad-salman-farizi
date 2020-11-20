36d6f5a1e1d772f9b91dc8c28c7f3eb9a8f5026c
salmonella-code <mohamad.salman.f2@gmail.com>
2020-11-20 17:23:16 +0700

    UTS update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+var slideIndex = [1,1];
+    var slideId = ["mySlides1", "mySlides2"]
+    showSlides(1, 0);
+    showSlides(1, 1);
+
+    function plusSlides(n, no) {
+      showSlides(slideIndex[no] += n, no);
+    }
+
+    function showSlides(n, no) {
+      var i;
+      var x = document.getElementsByClassName(slideId[no]);
+      if (n > x.length) {slideIndex[no] = 1}    
+      if (n < 1) {slideIndex[no] = x.length}
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+      }
+      x[slideIndex[no]-1].style.display = "block";  
+    }
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
@@ -10,4 +30,4 @@ function myFunction() {
         myDropdown.classList.remove('show');
       }
     }
-  }
+}
